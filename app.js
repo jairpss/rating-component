c042a00fbb993c1f5562335322aae52931a6a727
Jair Partida <jairpartida1@gmail.com>
2022-03-26 22:06:17 -0700

    thank you page done

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
 const select = () => {
     let element = document.getElementById("num5");
     element.classList.add("selected");
-    console.log("selected element");
 }
+
+const submit = () => {
+    let main = document.getElementById("main");
+    let thank = document.getElementById("thank-you");
+    main.classList.add("submit");
+    thank.classList.add("display");
+    console.log("hide");
+}
+
