d0bd16c50589f91732c17715a67a42ade06834d8
Jair Partida <jairpartida1@gmail.com>
2022-03-27 18:05:40 -0700

    trying to add class selected to clicked number

--- app.js
+++ app.js
@@ -1,12 +1,31 @@
+const rateNum = document.getElementsByClassName("rate-number")
+
+
 const select = () => {
-    let element = document.getElementById("num5");
-    element.classList.add("selected");
+    // rateNum.classList.add("selected")
+    
+
+    rateNum.addEventListener("click", function() {
+        rateNum.classList.add("selected")
+        console.log('selected number')
+      })
 }
+
+
+
+// let selectedRating = 5 
+// rateNum.forEach((rating, idx) =>{
+//     rating.addEventListener('click', () => {
+//         rating.classList.add('selected')
+//         selectedRating = idx + 1 
+//     } ) 
+
+// })
 
 const submit = () => {
     let main = document.getElementById("main");
     let thank = document.getElementById("thank-you");
-    main.classList.add("submit");
+    main.classList.add("hide");
     thank.classList.add("display");
     console.log("hide");
 }
