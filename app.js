0a8c5bb78020709faa6358dad77821975262ee3b
Jair Partida <jairpartida1@gmail.com>
2022-03-28 20:49:19 -0700

    app done

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-const rateNum = document.getElementsByClassName("rate-number")
+const rateNum = document.querySelectorAll(".rate-number")
+const selectedDiv = document.querySelector(".rate")
 
+let selectedNum = 3 
+rateNum.forEach((rating, idx) =>{
+    rating.addEventListener('click', () => {
+        removeSelected()
+        rating.classList.add('selected')
+        selectedNum = idx + 1 
+        console.log(selectedNum) 
+    } ) 
 
-const select = () => {
-    // rateNum.classList.add("selected")
-    
+})
 
-    rateNum.addEventListener("click", function() {
-        rateNum.classList.add("selected")
-        console.log('selected number')
-      })
+function removeSelected(){
+    rateNum.forEach((rating) => rating.classList.remove('selected'))
 }
-
-
-
-// let selectedRating = 5 
-// rateNum.forEach((rating, idx) =>{
-//     rating.addEventListener('click', () => {
-//         rating.classList.add('selected')
-//         selectedRating = idx + 1 
-//     } ) 
-
-// })
 
 const submit = () => {
     let main = document.getElementById("main");
     let thank = document.getElementById("thank-you");
     main.classList.add("hide");
     thank.classList.add("display");
+    selectedDiv.innerText = `${selectedNum}`
     console.log("hide");
 }
 
